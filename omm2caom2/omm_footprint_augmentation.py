9acf7c7b9d4400162d5ac5a7fb7afc07498ec2eb
SharonGoliath <goliaths.cadc@gmail.com>
2019-07-03 17:39:26 -0700

    s2503 - refactor OMM footprint calculation code into caom2pipe.

--- omm2caom2/omm_footprint_augmentation.py
+++ omm2caom2/omm_footprint_augmentation.py
@@ -70,9 +70,9 @@
 import logging
 import os
 
-from caom2 import Observation, CoordPolygon2D, ValueCoord2D, Chunk
+from caom2 import Observation
+from caom2pipe import caom_composable as cc
 from caom2pipe import manage_composable as mc
-from omm2caom2 import footprintfinder
 
 
 __all__ = ['visit']
@@ -110,79 +110,18 @@ def visit(observation, **kwargs):
     science_fqn = _unzip(science_fqn)
 
     count = 0
-    for i in observation.planes:
-        plane = observation.planes[i]
-        for j in plane.artifacts:
-            artifact = plane.artifacts[j]
-            for k in artifact.parts:
-                part = artifact.parts[k]
+    for plane in observation.planes.values():
+        for artifact in plane.artifacts.values():
+            for part in artifact.parts.values():
                 for chunk in part.chunks:
-                    _update_position(chunk, science_fqn)
+                    cc.exec_footprintfinder(
+                        chunk, science_fqn, log_file_directory,
+                        observation.observation_id)
                     count += 1
 
-    return_file = '{}_footprint.txt'.format(observation.observation_id)
-    return_string_file = '{}_footprint_returnstring.txt'.format(
-        observation.observation_id)
-    _handle_footprint_logs(log_file_directory, return_file)
-    _handle_footprint_logs(log_file_directory, return_string_file)
     logging.info('Completed footprint augmentation for {}'.format(
         observation.observation_id))
     return {'chunks': count}
-
-
-def _update_position(chunk, science_fqn):
-    """This function assumes that if the code got here, the science file is
-    on disk."""
-    logging.debug('Begin _update_position')
-    assert isinstance(chunk, Chunk), 'Expecting type Chunk'
-
-    if (chunk.position is not None
-            and chunk.position.axis is not None):
-        logging.debug('position exists, calculate footprints for {}.'.format(
-            science_fqn))
-        full_area, footprint_xc, footprint_yc, ra_bary, dec_bary, \
-            footprintstring, stc = footprintfinder.main(
-                '-r -f  {}'.format(science_fqn))
-        logging.debug('footprintfinder result: full area {} '
-                      'footprint xc {} footprint yc {} ra bary {} '
-                      'dec_bary {} footprintstring {} stc {}'.format(
-                        full_area, footprint_xc, footprint_yc, ra_bary,
-                        dec_bary, footprintstring, stc))
-        bounds = CoordPolygon2D()
-        coords = None
-        fp_results = stc.split('Polygon FK5')
-        if len(fp_results) > 1:
-            coords = fp_results[1].split()
-        else:
-            fp_results = stc.split('Polygon ICRS')
-            if len(fp_results) > 1:
-                coords = fp_results[1].split()
-
-        if coords is None:
-            raise mc.CadcException(
-                'Do not recognize footprint {}'.format(stc))
-
-        index = 0
-        while index < len(coords):
-            vertex = ValueCoord2D(mc.to_float(coords[index]),
-                                  mc.to_float(coords[index + 1]))
-            bounds.vertices.append(vertex)
-            index += 2
-            logging.debug('Adding vertex\n{}'.format(vertex))
-        chunk.position.axis.bounds = bounds
-    else:
-        logging.info('No position information for footprint generation.')
-    logging.debug('Done _update_position.')
-
-
-def _handle_footprint_logs(log_file_directory, log_file):
-    if log_file_directory is not None and os.path.exists(log_file_directory):
-        orig_log_fqn = os.path.join(os.getcwd(), log_file)
-        if os.path.exists(orig_log_fqn):
-            log_fqn = os.path.join(log_file_directory, log_file)
-            os.rename(orig_log_fqn, log_fqn)
-            logging.debug('Moving footprint log files from {} to {}'.format(
-                orig_log_fqn, log_fqn))
 
 
 def _unzip(science_fqn):
